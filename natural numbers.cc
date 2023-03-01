befe249ecaadd59de264c39930550a5b921bc4a9
17608katunge <123051164+17608katunge@users.noreply.github.com>
2023-03-01 15:54:34 +0300

    Add files via upload

---
+++ natural numbers.cc
@@ -0,0 +1,12 @@
+// program to display the first fifty natural numbers
+#include<stdio.h>
+int main ()
+{
+	int i;
+	for(i=0;i<51;i++);
+	{
+		printf("%d",i);
+	}
+	printf("for loop body finished");
+	return 0;
+}
---
+++ sum.cc
@@ -0,0 +1,12 @@
+// program to display the sum 
+#include
+int main ()
+{
+int n=15;
+while (n>9){
+printf("%d",n);
+n=n-1;
+}
+printf("loop body finished/n");
+return 0
+}
